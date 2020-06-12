23bb601c66a66ecb288cb7c8b046b9b3e168c9b5
PMahar <pmahar@techvalleyhigh.org>
2020-06-12 03:42:10 -0400

    Button positioning

--- Team-RPG/rpg.py
+++ Team-RPG/rpg.py
@@ -5,7 +5,7 @@ from sys import exit
 
 
 def parse():
-    button = '<button style="left: 5em; top: 5em" class="button" onclick="window.location=\'<!--BUTTON GOTO-->\'"><!--BUTTON LABEL--></button>'
+    button = '<button style="left: \'<!--L JUST-->\'; top: \'<!--T JUST-->\'" class="button" onclick="window.location=\'<!--BUTTON GOTO-->\'"><!--BUTTON LABEL--></button>'
 
     workingSheet = "sub.csv"
     print('READ SHEET:' + workingSheet)
@@ -17,10 +17,12 @@ def parse():
         with open(workingSheet) as csv_sheet:
             read_csv = csv.reader(csv_sheet, delimiter=',')
             for y in read_csv:
+                horizJ = 1
+                vertJ = 5
                 if rowCount != 0:
                     fileGen = open(y[0] + ".html", 'w')
                     for x in open(workingHTML, 'r'):
-                        # Begin counters for button goto and label later on
+                        # Begin counters for button goto, label, and position later on
                         i = 3
                         l = 4
                         if '<!--IMAGE HERE-->' in x:
@@ -30,26 +32,42 @@ def parse():
                             x = x.replace('<!--PROMPT HERE-->', y[1])
 
                         if '<!--PROMPT SIZE HERE-->' in x:
-                            if len(y[1]) > 0 and len(y[1]) < 150:
+                            if 0 < len(y[1]) < 150:
                                 subSize = "40"
-                            elif len(y[1]) > 150 and len(y[1]) < 250:
+                            elif 150 < len(y[1]) < 250:
                                 subSize = "30"
-                            elif len(y[1]) > 250 and len(y[1]) < 350:
+                            elif 250 < len(y[1]) < 350:
                                 subSize = "25"
                             x = x.replace('<!--PROMPT SIZE HERE-->', subSize + 'px')
 
                         if '<!--BUTTONS HERE-->' in x:
-                            print(len(y))
                             while l < len(y) and len(y[i]) > 0 and len(y[l]) > 0:
                                 if i > 3:
                                     x += "\n" + "	" + button
                                 else:
                                     x = x.replace('<!--BUTTONS HERE-->', '	' + button)
 
-                                print("i:" + str(i))
-                                print("l:" + str(l))
                                 x = x.replace('<!--BUTTON GOTO-->', y[i] + '.html')
                                 x = x.replace('<!--BUTTON LABEL-->', y[l])
+
+                                #If the remainder of the positional counters divided by 2 is zero (i.e., they're
+                                #even), use them for positioning. The counters inversely oscillate
+
+                                #Because we're generating buttons by rows, add to the vertical justify only when
+                                #we're done with one horizontal row at a time (So only increment horizontally
+                                #when vertJ isn't even)
+
+                                if horizJ % 2 != 0:
+                                    lJust = '1em'
+                                    horizJ += 1
+                                else:
+                                    lJust = '5em'
+                                    horizJ += 1
+                                    vertJ += 2
+
+                                x = x.replace('<!--L JUST-->', lJust)
+                                x = x.replace('<--T JUST-->', str(vertJ) + "em")
+
                                 i += 2
                                 l += 2
 
